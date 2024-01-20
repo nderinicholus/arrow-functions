df5e87d191e084252e398cc99de717a1265ba889
Tunerini <mwaniki.nderi@gmail.com>
2024-01-20 21:38:10 +0300

    Added even and odd nums examples

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 // Arrow functions
-const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-
-// Total
-const total = numbers.reduce((accumulator, element) => {
-  return accumulator + element;
-});
-console.log(`Total: ${total}`);
-
-// Average
-const average = numbers.reduce((accumulator, element) => {
-  return (accumulator + element) / numbers.length;
-})
-console.log(`Average: ${average.toFixed(2)}`)
+const numbers = [10, 15, 20, 30, 35, 40, 50, 55, 60, 70, 80, 90, 95];
 
 // Square
-const squares = numbers.map((element) => {
-  return Math.pow(element, 2);
-});
-
+const squares = numbers.map((element) => Math.pow(element, 2));
 console.log(`Square: ${squares}`);
 
 // Cubic
-const cubes = numbers.map((element) => {
-  return Math.pow(element, 3);
-});
+const cubes = numbers.map((element) => Math.pow(element, 3));
 console.log(`Cubic: ${cubes}`);
 
+// Even Numbers
+const evenNums = numbers.filter((element) => element % 2 === 0);
+console.log(`Even Numbers: ${evenNums}`);
 
+// Odd Numbers
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+console.log(`Odd Numbers: ${oddNums}`);
+
+// Total
+const total = numbers.reduce((accumulator, element) => accumulator + element);
+console.log(`Total: ${total}`);
+
+// Average
+const average = numbers.reduce(
+  (accumulator, element) => (accumulator + element) / numbers.length
+);
+console.log(`Average: ${average.toFixed(2)}`);
